59f6bb206466aa888d7871b7c1f7fcdf7ba42037
DESKTOP-E1P2LUQ\kimsohyun <kim.159657@gmail.com>
2020-02-04 11:09:19 +0900

    test commit

--- app/src/main/java/com/example/example01/JoinActivity.java
+++ app/src/main/java/com/example/example01/JoinActivity.java
@@ -17,7 +17,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+// test
 public class JoinActivity extends AppCompatActivity implements View.OnClickListener {
 
     //define view objects
